8e39ce0492b4036fd141a7f5b8fcfb1a79222467
Payal Khandelwal <khandelw004@mymail.sim.edu.sg>
2022-09-05 12:50:41 +0800

    tweets api fix

--- services/cryptoTweetsDataService.js
+++ services/cryptoTweetsDataService.js
@@ -5,7 +5,15 @@ export const getTweetsData = async (curr) => {
 
     var id = curr.id.toLowerCase()
     var coin = curr.item.toLowerCase()
-    let result = id.concat('-',coin);
+    let result = ""
+    if (coin.split(" ").length > 1) {
+      let w1 = coin.split(" ")[0]
+      let w2 = coin.split(" ")[1]
+      result = id.concat('-', w1.concat('-', w2))
+    } else {
+      result = id.concat('-',coin);
+    }
+    console.log(result)
     const response = await axios.get(`https://api.coinpaprika.com/v1/coins/${result}/twitter` , { data:null}, {
   headers: {
     'Aceept': 'application/json',
